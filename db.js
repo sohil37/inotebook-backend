84ccb5e7e6f5050e8ad242132fe6c9b013c396f5
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-04-07 17:11:20 +0530

    initial commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb://0.0.0.0:27017"
+
+const connectToMongoDB = () => {
+    mongoose.connect(mongoURI).then(
+        ()=>{
+            console.log("Connected to MongoDB.")
+        }
+    )
+}
+module.exports = connectToMongoDB;
