d0f8dcc8a26b7b58774be7c9053f787994efb5cd
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-04-08 12:21:16 +0530

    added collection to thunderclient and refactored the code.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const connectToMongoDB = require("./db");
 connectToMongoDB();
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,25 +3,38 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
+// endpoint to create a new user
 router.post(
-  "/",
+  "/createUser",
   [
     body("email", "enter a valid email.").isEmail(),
     body("name", "enter a valid name.").isLength({ min: 2 }),
     body("password", "enter a valid password.").isLength({ min: 8 }),
   ],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+  async (req, res) => {
+    try {
+      // if any error in validation, return bad request
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      // check if user with same email already exist
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry, a user with same email already exist" });
+      }
+      // create new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json({ message: "User created successfully.", user: user });
+    } catch (err) {
+      res.status(500).send("Some error occured.");
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => res.json(err.message));
   }
 );
 
