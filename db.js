90446ad03555f15974f7121d6d06bd75376b6fdb
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-04-08 11:57:17 +0530

    added validation

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://0.0.0.0:27017"
+const mongoURI = "mongodb://0.0.0.0:27017/inotebook";
 
 const connectToMongoDB = () => {
-    mongoose.connect(mongoURI).then(
-        ()=>{
-            console.log("Connected to MongoDB.")
-        }
-    )
-}
+  mongoose.connect(mongoURI).then(() => {
+    console.log("Connected to MongoDB.");
+  });
+};
 module.exports = connectToMongoDB;
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("email", "enter a valid email.").isEmail(),
+    body("name", "enter a valid name.").isLength({ min: 2 }),
+    body("password", "enter a valid password.").isLength({ min: 8 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => res.json(err.message));
+  }
+);
 
 module.exports = router;
