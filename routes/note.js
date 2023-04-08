7f8009844a1c996bcc0527a69b599144ceeef793
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-04-08 16:59:23 +0530

    added updateNote endpoint

--- routes/note.js
+++ routes/note.js
@@ -39,4 +39,48 @@ router.get(
   }
 );
 
+// endpoint to update a note by logged in user
+router.put(
+  "/updateNote/:id",
+  fetchUser,
+  [
+    body("title", "enter a valid title."),
+    body("description", "enter a valid description.").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      // check if note already exist
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("Not found.");
+      }
+      // check if user is a valid user
+      if (note.user.toString() != req.user.id) {
+        return res.status(401).send("Not allowed");
+      }
+      // update note
+      const { title, description, tag } = req.body;
+      const newNote = {};
+      if (title) {
+        newNote.title = title;
+      }
+      if (description) {
+        newNote.description = description;
+      }
+      if (tag) {
+        newNote.tag = tag;
+      }
+      const updatedNote = await Note.findByIdAndUpdate(
+        req.params.id,
+        { $set: newNote },
+        { new: true }
+      );
+      res.json(updatedNote);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Some error occured.");
+    }
+  }
+);
+
 module.exports = router;
